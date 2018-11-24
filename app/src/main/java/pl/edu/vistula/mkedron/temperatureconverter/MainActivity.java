eba8bd402a6d53b5f884628fa8f1116f9533c131
mkedron <mkkedron@gmail.com>
2018-11-24 11:31:43 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/vistula/mkedron/temperatureconverter/MainActivity.java
@@ -0,0 +1,39 @@
+package pl.edu.vistula.mkedron.temperatureconverter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View view) {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.tempRarioGroup);
+        EditText numberField = (EditText)findViewById(R.id.tempValueField);
+        Float tempValue = Float.valueOf(numberField.getText().toString());
+        if(radioGroup.getCheckedRadioButtonId() == R.id.CelsiusRadioButton) {
+            tempValue = convertFahtr2Cel(tempValue);
+        } else {
+            tempValue = convertCel2Fahr(tempValue);
+        }
+
+        numberField.setText(tempValue.toString());
+
+    }
+
+    private Float convertCel2Fahr(Float celValue) {
+        return ((celValue * 9) / 5) + 32;
+    }
+
+    private Float convertFahtr2Cel(Float fahrValue) {
+        return ((fahrValue - 32) * 5 / 9);
+    }
+}
